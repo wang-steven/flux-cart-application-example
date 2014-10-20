cfe62828d1fa2d83685b6be25562892ce4a2b81b
wang-steven <atpqq0525@gmail.com>
2014-10-21 00:29:35 +0800

    flux application example

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-/** @jsx React.DOM */
-'use strict';
-var APP = require('./components/app'),
-    React = require('react');
-
-/* jshint ignore:start */
-React.renderComponent(<APP />, document.getElementById('main'));
-/* jshint ignore:end */
